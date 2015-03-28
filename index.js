6ad42f404e041f5be46a7d51bf4b1467139dbcf9
Yann Lombard <contact@yannlombard.com>
2015-03-28 14:29:31 +0100

    test

--- index.js
+++ index.js
@@ -5,13 +5,12 @@
 
     var fs = require('fs'),
         client = require('./drone'),
-        capture = require('./capture')(client);
+        capture = require('./capture')(client),
+        move = require('./move'),
+        total = 30;
 
-    require('./move')(client, function (position) {
+    move(client, total, function(err, position) {
         console.log('TAKE PICTURE IN POSITION', position);
     });
-
-    //    client.createREPL();
-
 
 }());
--- move.js
+++ move.js
@@ -1,8 +1,5 @@
 var fs = require('fs');
 
-
-//client.takeoff();
-
 /*client
  .after(5000, function() {
  this.clockwise(0.5);
@@ -12,16 +9,63 @@ var fs = require('fs');
  this.land();
  });*/
 
-module.exports = function(client) {
 
-    client.getPngStream()
-        .on('data', function(data) {
-            var now = new Date().getTime();
-            var fileName = './photos/' + now + '.png';
+/*client.getPngStream()
+ .on('data', function(data) {
+ var now = new Date().getTime();
+ var fileName = './photos/' + now + '.png';
 
-            fs.writeFile(fileName, data, function(err) {
-                if(err) console.log(err);
-                console.log(fileName + ' Saved');
-            });
-        });
+ fs.writeFile(fileName, data, function(err) {
+ if(err) console.log(err);
+ console.log(fileName + ' Saved');
+ });
+ });*/
+
+
+var getPoints = function() {
+
+    /*var pts = [];
+    var rad = 0.5;
+    var center = {
+        x: .5,
+        y: .5
+    };
+
+    for (var i = 0; i <= 36; i++) {
+        var a = i * 10 * Math.PI / 180;
+        var p = new L.Point(Math.cos(a), Math.sin(a));
+        this.normalize(p, rad);
+        p = p._add({x: center.x, y: center.y});
+        pts.push(p);
+    }
+
+
+    normalize: function(point, scale) {
+        var norm = Math.sqrt(point.x * point.x + point.y * point.y);
+        if(norm != 0) { // as3 return 0,0 for a point of zero length
+            point.x = scale * point.x / norm;
+            point.y = scale * point.y / norm;
+        }
+    }*/
+
+};
+
+module.exports = function(client, total, cb) {
+    var position = 0;
+
+    //client.takeoff();
+
+    // take first picture
+    var move = function() {
+        cb(null, position);
+
+        setTimeout(function() {
+            position++;
+            if(position < total) {
+                move();
+            }
+        }, 100);
+    };
+
+    move();
 };
